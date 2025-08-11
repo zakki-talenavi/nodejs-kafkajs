ab774425a0f1368fc1ab973eb9d30864461280db
Zakki Haris <92501896+zakkiharis@users.noreply.github.com>
2025-08-11 17:22:53 +0700

    change topic consumer

--- consumer.js
+++ consumer.js
@@ -1,23 +1,23 @@
-import {Kafka} from "kafkajs";
+import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
-    "brokers" : ["localhost:9092"]
+  brokers: ["localhost:9092"],
 });
 
 const consumer = kafka.consumer({
-    groupId: "nodejs"
+  groupId: "nodejs",
 });
 
 await consumer.subscribe({
-    topic: "helloworld",
-    fromBeginning: true
+  topic: "notification-topic",
+  fromBeginning: true,
 });
 
 await consumer.connect();
 
 await consumer.run({
-    eachMessage: async (record) => {
-        const message = record.message;
-        console.info(message.value.toString())
-    }
+  eachMessage: async (record) => {
+    const message = record.message;
+    console.info(message.value.toString());
+  },
 });
